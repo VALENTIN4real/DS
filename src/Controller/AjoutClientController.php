ad9c8500c5bbe0e7cc3aa52fff1c3e8a9fb24309
Mairesse Valentin <mairesse.valentin.pro@gmail.com>
2020-10-06 17:49:45 +0000

    liste clients OK

--- src/Controller/AjoutClientController.php
+++ src/Controller/AjoutClientController.php
@@ -38,4 +38,54 @@ class AjoutClientController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/liste", name="liste")
+     */
+    public function listeClients(Request $request)
+    {
+        $em = $this->getDoctrine();
+        $repoClient = $em->getRepository(Client::class);   
+
+        $clients = $repoClient->findBy(array(),array('nomClient'=>'ASC'));
+        return $this->render('client/liste_clients.html.twig', [
+           'clients'=>$clients
+        ]);
+    }
+
+
+      /**
+     * @Route("/modif/{id}", name="modif", requirements={"id"="\d+"})
+     */
+    public function modifClient(int $id, Request $request)
+    {
+        $em = $this->getDoctrine();
+        $repoClient = $em->getRepository(Client::class);
+        $client = $repoClient->find($id);
+
+        if($client==null){
+            $this->addFlash('notice', "Ce client n'existe pas");
+            return $this->redirectToRoute('liste_client');
+        }
+
+        $form = $this->createForm(ModifClientType::class,$theme);
+
+        if ($request->isMethod('POST')) {            
+            $form->handleRequest($request);            
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($client);
+                $em->flush();    
+
+                $this->addFlash('notice', 'client modifié');
+
+            }
+            return $this->redirectToRoute('liste_client');
+        }        
+
+        return $this->render('client/modif_client.html.twig', [
+            'form'=>$form->createView()
+        ]);
+    }
+
+
 }
